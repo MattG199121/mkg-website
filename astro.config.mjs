19e8f7fcd3c2e79bbecc1b524c99c940477c4f94
MattG199121 <2mcsrrf7b9@privaterelay.appleid.com>
2026-08-09 21:12:18 +0100

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mattg199121.github.io',
+  base: '/mkg-website',
+  trailingSlash: 'always',
+});
